265405f1e7e190356e8f24366c669172403692af
SpirosAvg <50573405+SpirosAvg@users.noreply.github.com>
2020-10-02 13:58:47 +0300

    solution r programming

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Set and get the value of matrix 
+# set and get inverse matrix 
 
+
+makeCacheMatrix <- function(    x = matrix()    ) 
+{
+    invr <- NULL
+    set <- function(y) {
+        x <<- y
+        invr <<- NULL
+    }
+    get <- function() x
+    gettinginverse <- function() invr
+    settinginverse <- function(inverse) invr <<- inverse
+    list(set=set, get=get, settinginverse=settinginverse, gettinginverse=gettinginverse)
 }
 
-
 ## Write a short comment describing this function
+## Calculates inverse matrix 
+# if the result is cached it skips the computation and goes forward getting the cacheed result 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invr <- x$gettinginverse()
+    if(!is.null(invr)) {
+        message("getting cached data.")
+        return(invr)
+    }
+    data <- x$get()
+    invr <- solve(data)
+    x$settinginverse(inv)
+    invr
 }
